8d4358b382628fae2346cb5493638eedcdf12d02
dakotarobot213 <dakotarobot213@gmail.com>
2023-11-28 12:32:47 -0700

    Syncing work, nonfunctional

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TourList from "./components/TourList";
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let tours = Array.from(data);
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img
+					src={logo}
+					className='App-logo'
+					alt='logo'
+				/>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+			<div></div>
+			<TourList data={tours} />
+		</div>
+	);
 }
 
 export default App;

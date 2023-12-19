e8d918350a06b32231dc32db2354c2aa518c73e9
dakotarobot213 <dakotarobot213@gmail.com>
2023-12-19 10:04:59 -0700

    Pruned unnecessary code and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import "./App.css";
-import "./Styles/css/main.css";
-import GuideList from "./Components/GuideList";
+import { useState } from "react";
+import "./styles/css/main.css";
+import TourList from "./components/TourList";
 import data from "./data";
 
 function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-			<GuideList data={data} />
-		</div>
-	);
+	return <TourList data={data} />;
 }
 
 export default App;
